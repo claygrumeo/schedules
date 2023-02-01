0888bd2bfe35d1ec6289fd1cb44c49bde1654dab
Clay Grumieaux <claygrumeo@gmail.com>
2023-02-01 09:55:13 -0500

    calendar work

--- apps/frontend/src/app/components/booking/dateTime/calendar/calendar.tsx
+++ apps/frontend/src/app/components/booking/dateTime/calendar/calendar.tsx
@@ -1,8 +1,70 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from 'react';
 import { useState, useEffect } from 'react';
+import { DateTime } from 'luxon';
+import './calendar.scss';
 
-export default function Calendar({ selectDate }: {selectDate: (selection: string) => void}) {
-  return(
-    <div onClick={(e) => selectDate("2023-01-31")}>Calendar</div>
-  )
+export default function Calendar({
+  month,
+  selectedDate,
+  selectDate,
+  advanceMonth,
+  rewindMonth,
+}: {
+  month: DateTime;
+  selectedDate: string;
+  selectDate: (selection: string) => void;
+  advanceMonth: () => void;
+  rewindMonth: () => void;
+}) {
+  const [calendarDays, setCalendarDays] = useState<DateTime[]>([]);
+
+  // When the month changes, recalculate the dates to show on the calendar
+  useEffect(() => {
+    // Generate the current month's days
+    const newCalendarDays = Array.from({ length: month.daysInMonth }, (_, i) =>
+      month.startOf('month').plus({ days: i })
+    );
+    // Generate
+    const numLeadingDays =
+      month.startOf('month').weekday === 7 ? 0 : month.startOf('month').weekday;
+    const numTrailingDays =
+      month.endOf('month').weekday === 7 ? 6 : 6 - month.endOf('month').weekday;
+    if (numLeadingDays > 0) {
+      const previousMonthLastDay = month.minus({ months: 1 }).endOf('month');
+      for (let i = 0; i < numLeadingDays; i++) {
+        newCalendarDays.unshift(previousMonthLastDay.minus({ days: i }));
+      }
+    }
+    if (numTrailingDays > 0) {
+      const nextMonthFirstDay = month.plus({ months: 1 }).startOf('month');
+      for (let i = 0; i < numTrailingDays; i++) {
+        newCalendarDays.push(nextMonthFirstDay.plus({ days: i }));
+      }
+    }
+    setCalendarDays(newCalendarDays);
+  }, [month]);
+
+  return (
+    <div className="calendarContainer">
+      <div className="calendarButtonsContainer">
+        <button onClick={(e) => rewindMonth()}>Prev</button>
+        <h2>{month.toFormat('MMMM yyyy')}</h2>
+        <button onClick={(e) => advanceMonth()}>Next</button>
+      </div>
+      <div className="dateGrid">
+        {calendarDays.map((day, i) => {
+          return (
+            <button
+              className={`calendarDay ${selectedDate === day.toFormat('yyyy-MM-dd') ? 'selected' : ""}`}
+              disabled={month.month !== day.month}
+              onClick={() => selectDate(day.toFormat('yyyy-MM-dd'))}
+              key={i}
+            >
+              {day.toFormat('d')}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- apps/frontend/src/app/components/booking/dateTime/dateTime.tsx
+++ apps/frontend/src/app/components/booking/dateTime/dateTime.tsx
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import Calendar from "./calendar/calendar";
-import BookableSummary from "../bookableSummary/bookableSummary";
-import { Bookable } from "@frontend/types";
-import './dateTime.scss'
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Calendar from './calendar/calendar';
+import BookableSummary from '../bookableSummary/bookableSummary';
+import { Bookable } from '@frontend/types';
+import { DateTime } from 'luxon';
+import './dateTime.scss';
 
-export default function DateTime() {
-  const [selectedDate, setSelectedDate] = useState("")
+export default function TimeSelection() {
+  const [selectedDate, setSelectedDate] = useState(
+    DateTime.now().toFormat('yyyy-MM-dd')
+  );
+  const [currentMonth, setCurrentMonth] = useState(DateTime.now());
   // Get the bookable from Route info
   const location = useLocation();
-  const bookable: Bookable = location.state?.bookable
+  const bookable: Bookable = location.state?.bookable;
 
   const selectDate = (selection: string) => {
-    setSelectedDate(selection)
-  }
+    setSelectedDate(selection);
+  };
 
-  return(
+  const advanceMonth = () => {
+    setCurrentMonth(currentMonth.plus({ months: 1 }));
+  };
+
+  const rewindMonth = () => {
+    setCurrentMonth(currentMonth.minus({ months: 1 }));
+  };
+
+  return (
     <div className="dateTimeWrapper">
       <BookableSummary bookable={bookable} />
-      {`Selected Date: ${selectedDate}`}
-      <Calendar selectDate={selectDate}/>
+      <Link className="backToAppointments" to={'/book'}>Back to Appointments</Link>
+      {/* {`Selected Month: ${currentMonth.toFormat('MMMM yyyy')}`}
+      <hr />
+      {`Selected Date: ${selectedDate}`} */}
+      <Calendar
+        month={currentMonth}
+        selectedDate={selectedDate}
+        selectDate={selectDate}
+        advanceMonth={advanceMonth}
+        rewindMonth={rewindMonth}
+      />
     </div>
-  )
+  );
 }
